2648685cd577545108480dcc06182c6a32887297
Satria-WS <extencezone02@gmail.com>
2024-10-01 11:27:33 +0700

    add route to dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
@@ -21,6 +22,10 @@ export const routes: Routes = [
         path: 'register',
         component: RegisterComponent,
       },
+      {
+        path: 'dashboard',
+        component:DashboardComponent
+      }
     ],
   },
   // {
