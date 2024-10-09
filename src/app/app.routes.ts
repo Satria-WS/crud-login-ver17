bcd3458236b0e5d0f6dee023eca94789b905819f
Satria-WS <extencezone02@gmail.com>
2024-10-09 14:06:32 +0700

    router list add edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './guard/auth.guard';
+import { AddComponent } from './pages/dashboard/add/add.component';
+import { EditComponent } from './pages/dashboard/edit/edit.component';
+import { ListComponent } from './pages/dashboard/list/list.component';
 
 export const routes: Routes = [
   {
@@ -26,8 +29,28 @@ export const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent,
-        canActivate: [authGuard]
-      }
+        canActivate: [authGuard],
+        children: [
+          {
+            path: '',
+            redirectTo: 'list',
+            pathMatch: 'full',
+          },
+
+          {
+            path: 'list',
+            component: ListComponent,
+          },
+          {
+            path: 'add',
+            component: AddComponent,
+          },
+          {
+            path: 'edit',
+            component: EditComponent
+          }
+        ],
+      },
     ],
   },
   // {
