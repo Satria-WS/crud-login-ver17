e6e10c9b479403a9f42710b86180febd67bbf49c
Satria-WS <extencezone02@gmail.com>
2024-10-15 10:38:18 +0700

    add form logic and refactoring

--- src/app/pages/dashboard/add/add.component.ts
+++ src/app/pages/dashboard/add/add.component.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CrudService, List } from '../../../services/crud.service';
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './add.component.html',
-  styleUrl: './add.component.css'
+  styleUrl: './add.component.css',
 })
-export class AddComponent {
+export class AddComponent implements OnInit {
+  addForm: FormGroup | any;
+  showList: List[];
+  formSubmitted: boolean = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private crudService: CrudService
+  ) {
+    this.showList = crudService.listApp;
+    this.addForm = this.createForm();
+  }
+
+  ngOnInit(): void {
+    console.log(this.showList);
+  }
+
+  private createForm(): FormGroup {
+    return this.fb.group({
+      title: ['', [Validators.required]],
+      detail: ['', Validators.required],
+    });
+  }
+
+  private addList(formData: { title: string; detail: string }): void {
+    const newItem: List = {
+      _id: this.showList.length + 1,
+      title: formData.title,
+      text: formData.detail,
+    };
+    this.showList.push(newItem);
+    console.log('Updated showList:', this.showList);
+  }
+
+  onSubmit(): void {
+    const formData = this.addForm.value;
+    this.formSubmitted = true;
+
+    if (this.addForm.valid) {
+      this.addList(formData);
+      this.addForm.reset();
+    } else {
+      console.warn('form is invalid');
+    }
+  }
 }
