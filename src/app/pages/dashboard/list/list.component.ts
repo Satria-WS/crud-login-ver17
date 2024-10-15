1a32933089781ea9ce73ea15bdf1238071163ccb
Satria-WS <extencezone02@gmail.com>
2024-10-15 10:38:54 +0700

    add service from crudService

--- src/app/pages/dashboard/list/list.component.ts
+++ src/app/pages/dashboard/list/list.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CrudService, List } from '../../../services/crud.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink , ReactiveFormsModule],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
+  showList: List[];
 
+  constructor(private crudService: CrudService) {
+    this.showList = this.crudService.listApp;
+   }
+
+  // showList() {
+  //   const showList = this.crudService.listApp;
+  //   console.log(showList);
+  //   return showList;
+  // }
 }
