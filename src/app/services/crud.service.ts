8effa0f9363a5d364f8a8725b6035ab66776c58f
Satria-WS <extencezone02@gmail.com>
2024-10-16 14:40:41 +0700

    adding crud service for add and delete

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -16,4 +16,25 @@ export class CrudService {
     { _id: 2, title: 'sample_2', text: 'Todo sample_2' },
     { _id: 3, title: 'sample_3', text: 'Todo sample_3' },
   ];
+
+  addList(formData: { title: string; detail: string }): void {
+    const newItem: List = {
+      _id: this.listApp.length + 1,
+      title: formData.title,
+      text: formData.detail,
+    };
+    this.listApp.push(newItem);
+    console.log('Updated showList:', this.listApp);
+  }
+
+  deleteList(id: number): void {
+    const findIndex = this.listApp.findIndex(index => index._id === id);
+
+    if (findIndex !== -1) {
+      this.listApp.splice(findIndex, 1);
+    } else {
+      console.warn('id tidak di temukan');
+    }
+  }
+
 }
