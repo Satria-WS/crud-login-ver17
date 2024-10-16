87651c531a610cfaa07a9e1b83c79a8fe77de68b
Satria-WS <extencezone02@gmail.com>
2024-10-16 14:49:49 +0700

    update the logic for update

--- src/app/pages/dashboard/edit/edit.component.ts
+++ src/app/pages/dashboard/edit/edit.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
+import { CrudService } from '../../../services/crud.service';
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.css'
 })
 export class EditComponent {
+  updateForm: FormGroup | any;
+  formSubmitted: boolean = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private crudService : CrudService
+  ) {
+
+  }
+
+  onSubmit() {
+    console.log('update here');
+  }
 }
