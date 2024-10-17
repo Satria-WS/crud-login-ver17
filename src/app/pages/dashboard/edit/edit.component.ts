ceebfe4e038a78c796ca28affea1fa92d8c5345f
Satria-WS <extencezone02@gmail.com>
2024-10-17 12:57:20 +0700

    add logic list by id

--- src/app/pages/dashboard/edit/edit.component.ts
+++ src/app/pages/dashboard/edit/edit.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import {
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms'
-import { CrudService } from '../../../services/crud.service';
+import { CrudService, List } from '../../../services/crud.service';
+
+
 @Component({
   selector: 'app-edit',
   standalone: true,
@@ -14,18 +16,57 @@ import { CrudService } from '../../../services/crud.service';
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.css'
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
   updateForm: FormGroup | any;
   formSubmitted: boolean = false;
+  itemId!: number; // '!' This tells TypeScript that itemId will be assigned later
+  item: List | undefined;
 
   constructor(
     private fb: FormBuilder,
-    private crudService : CrudService
+    private crudService: CrudService,
+    private routeById : ActivatedRoute
   ) {
+    this.updateForm = this.editForm();
+  }
 
+  ngOnInit(): void {
+
+    //get Id
+    this.routeById.params.subscribe(params => {
+      this.itemId = +params['id'];
+      this.item = this.crudService.listApp.find(item => item._id === this.itemId);
+      if (this.item) {
+        this.updateForm.patchValue({
+          title: this.item.title,
+          detail: this.item.text
+        })
+      }
+    });
+
+    console.log('onInit');
+  }
+
+  private editForm(): FormGroup {
+    return this.fb.group({
+      title: ['', Validators.required],
+      detail: ['', Validators.required]
+    })
   }
 
   onSubmit() {
+    const formData = this.updateForm.value;
+    // const findId = this.crudService.listApp.find(index => index._id === id);
+    this.formSubmitted = true;
+
+
+    if (this.updateForm.valid) {
+      this.formSubmitted = false;
+      this.crudService.updateList(this.itemId, formData);
+
+      console.log(formData);
+    }
+
     console.log('update here');
   }
 }
